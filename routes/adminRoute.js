97b6f7fd9b5365a56f89265110f6bf6397e875c3
anithav <anithav0909@gmail.com>
2023-01-06 15:05:17 +0530

    file updated

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -102,7 +102,7 @@ var upload = multer({ storage: storage })
 
 
 // addpizza data
-router.post("/addpizza", verifyToken, upload.single('file'), (req, res, next) => {
+router.add("/addpizza", verifyToken, upload.single('file'), (req, res, next) => {
     var file = req.file
     var pizza = new Pizza({
         pizzaname: req.body.pizzaname,
